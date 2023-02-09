566a1466ac144c1f497042392eacac778210ad1c
Kuricyn Aleksej <al.curitsin@gmail.com>
2023-02-09 15:16:07 +0500

    Add class LocationViewModel

---
+++ src/Core/ClientExplorer.Shared.ViewModels/Entities/LocationEntities/LocationViewModel.cs
@@ -0,0 +1,8 @@
+namespace ClientExplorer.Shared.ViewModels;
+
+public sealed class LocationViewModel
+{
+  public string? LocationName { get; set; }
+  public string? StreetName { get; set; }
+  public string? HouseNumber { get; set; }
+}
