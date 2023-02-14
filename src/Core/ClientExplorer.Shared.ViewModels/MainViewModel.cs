4a3c31eb563095506197a828996175c2aa609f4c
Kuricyn Aleksej <al.curitsin@gmail.com>
2023-02-14 16:38:14 +0500

    Add LoadClientLocation() Чтение списка объектов у клиента.

--- src/Core/ClientExplorer.Shared.ViewModels/MainViewModel.cs
+++ src/Core/ClientExplorer.Shared.ViewModels/MainViewModel.cs
@@ -11,12 +11,13 @@ public class MainViewModel : BaseViewModel
   #region Public Properties
 
   public string StatusInfo { get; set; }
-  
+
   public ObservableCollection<ClientEntityViewModel> SortedClients { get; set; }
   public ClientEntityViewModel SelectedClient { get; set; }
-  
+
   public ObservableCollection<string> SortedLocation { get; set; }
-  
+
+
   public ICommand OpenClient { get; }
 
   #endregion
@@ -33,28 +34,54 @@ public class MainViewModel : BaseViewModel
 
   public MainViewModel()
   {
-
     OpenClient = new DelegateCommand(Open);
-    
+
     SortedClients = new ObservableCollection<ClientEntityViewModel>();
+    SortedLocation = new ObservableCollection<string>();
+
     //ClientEr.CurrentPath = AppDomain.CurrentDomain.BaseDirectory;
     //TODO: Хардкодим путь до папок с клиентами на время разработки и тестирования. В будущем значение будет вынесено в файл настроек "*.ini". 
     ClientEr.CurrentPath = "/mnt/share/Clients";
-    
+
     StatusInfo = ClientEr.CurrentPath;
 
     GetClientList();
-
   }
 
   private void Open(object? parametr)
   {
-    
     LoadClientLocation();
   }
 
   private void LoadClientLocation()
   {
+    SortedLocation.Clear();
+
+    if (SelectedClient.ClientPath == null)
+    {
+      StatusInfo = "Err: SelectedClient.ClientPath == null";
+      return;
+    }
+    
+    //TODO Имя папки "объекты" нужно вывести в свойство!
+    var directoryPath = SelectedClient.ClientPath.FullName + Path.DirectorySeparatorChar + "Объекты";
+
+    if (!Directory.Exists(directoryPath))
+    {
+      StatusInfo = "Err: 'Объекты' не обнаружены";
+      return;
+    }
+
+    var directoryInfo = new DirectoryInfo(directoryPath);
+
+    foreach (var directory in directoryInfo.GetDirectories())
+    {
+      SortedLocation.Add(directory.Name);
+    }
+
+    SortedLocation = new ObservableCollection<string>(SortedLocation.OrderBy(i => i));
+
+    StatusInfo = SelectedClient.ClientPath.FullName;
     
   }
 
@@ -70,13 +97,13 @@ public class MainViewModel : BaseViewModel
   {
     if (ClientEr.CurrentPath == null)
     {
-      StatusInfo = "Err: CurrentPath = null";
+      StatusInfo = "Err: ClientEr.CurrentPath == null";
       return;
     }
-    
+
     SortedClients.Clear();
-   
-    var directoryInfo =new DirectoryInfo(ClientEr.CurrentPath);
+
+    var directoryInfo = new DirectoryInfo(ClientEr.CurrentPath);
 
     foreach (var directory in directoryInfo.GetDirectories())
     {
@@ -85,6 +112,6 @@ public class MainViewModel : BaseViewModel
 
     SortedClients = new ObservableCollection<ClientEntityViewModel>(SortedClients.OrderBy(i => i.Name));
   }
-  
+
   #endregion
 }
