b64a669c676d6a5f2acce1800e94c9433c02727f
Kuricyn Aleksej <al.curitsin@gmail.com>
2023-02-21 16:43:09 +0500

    Add ordered Address Locations on load.

--- src/Core/ClientExplorer.Shared.ViewModels/AddressLocationViewModel.cs
+++ src/Core/ClientExplorer.Shared.ViewModels/AddressLocationViewModel.cs
@@ -11,58 +11,90 @@ namespace ClientExplorer.Shared.ViewModels;
 
 public class AddressLocationViewModel : BaseViewModel
 {
-    public AddressLocationViewModel()
+  private class AddressLocForOrdered
+  {
+    public AddressLocationEntityViewModel Loc { get; }
+    public string OrderProp { get; }
+
+    public AddressLocForOrdered(AddressLocationEntityViewModel _loc)
     {
-        _directoryDataResourcePath =
-            ClientEr.CurrentPath + Path.DirectorySeparatorChar + ClientEr.DefaultDataResourcePath;
-        _locationsFilePath = _directoryDataResourcePath + Path.DirectorySeparatorChar + ClientEr.LocationsSourceFileName;
-        FillAddressLocations();
+      Loc = _loc;
+      OrderProp = _loc.CityName.ToUpper() + "~" + _loc.StreetName.ToUpper() + "~" + _loc.HouseNumber.ToUpper();
     }
-    
-    public ObservableCollection<AddressLocationEntityViewModel>? AddressLocations { get; set; } =
-        new ObservableCollection<AddressLocationEntityViewModel>();
+  }
 
-    private readonly string _locationsFilePath;
-    private readonly string _directoryDataResourcePath;
+  public AddressLocationViewModel()
+  {
+    _directoryDataResourcePath =
+      ClientEr.CurrentPath + Path.DirectorySeparatorChar + ClientEr.DefaultDataResourcePath;
+    _locationsFilePath = _directoryDataResourcePath + Path.DirectorySeparatorChar + ClientEr.LocationsSourceFileName;
+    FillAddressLocations();
 
-    private void FillAddressLocations()
+    // Упорядочить список по Алфавиту. Город+Улица+НомерДома.
+    List<AddressLocForOrdered> addressLocsForOrdered = new System.Collections.Generic.List<AddressLocForOrdered>();
+
+    foreach (var addressLocation in AddressLocations)
     {
-        using (FileStream fs = new FileStream(_locationsFilePath, FileMode.OpenOrCreate, FileAccess.Read))
-        {
-            AddressLocations = JsonSerializer.Deserialize<ObservableCollection<AddressLocationEntityViewModel>>(fs);
-        }
+      addressLocsForOrdered.Add(new AddressLocForOrdered(addressLocation));
     }
-    // private async Task FillAddressLocations()
-    // {
-    //     using (FileStream fs = new FileStream(_locationsFilePath, FileMode.OpenOrCreate, FileAccess.Read))
-    //     {
-    //         //AppData.Locations = await JsonSerializer.DeserializeAsync<ListLocations>(fs); //Это не работало
-    //         AddressLocations = await JsonSerializer.DeserializeAsync<ObservableCollection<AddressLocationEntityViewModel>>(fs);
-    //
-    //         //MessageBox.Show(string.Format("Count: {0}", AppData.Locations.Locations.Count)); //Тут поле пустое == null
-    //         //ListLocations? loc = await JsonSerializer.DeserializeAsync<ListLocations>(fs);
-    //         //Console.WriteLine(loc.Locations.Count.ToString());
-    //     }
-    // }
 
-    private async Task UploadAddressLocations()
+    addressLocsForOrdered.OrderBy(i => i.OrderProp);
+
+    AddressLocations.Clear();
+
+    foreach (var addressLoc in addressLocsForOrdered)
     {
-        if (!Directory.Exists(_directoryDataResourcePath))
-        {
-            Directory.CreateDirectory(_directoryDataResourcePath);
-        }
-
-        JsonSerializerOptions options = new JsonSerializerOptions
-        {
-            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
-            WriteIndented = true
-        };
-
-        using (FileStream fs = new FileStream(_locationsFilePath, FileMode.OpenOrCreate))
-        {
-            if (AddressLocations != null)
-                await JsonSerializer.SerializeAsync<ObservableCollection<AddressLocationEntityViewModel>>(fs, AddressLocations,
-                    options);
-        }
+      AddressLocations.Add(new AddressLocationEntityViewModel()
+      {
+        CityName = addressLoc.Loc.CityName,
+        StreetName = addressLoc.Loc.StreetName,
+        HouseNumber = addressLoc.Loc.HouseNumber
+      });
     }
+  }
+
+  public ObservableCollection<AddressLocationEntityViewModel>? AddressLocations { get; private set; } =
+    new ObservableCollection<AddressLocationEntityViewModel>();
+
+  private readonly string _locationsFilePath;
+  private readonly string _directoryDataResourcePath;
+
+  private void FillAddressLocations()
+  {
+    using FileStream fs = new FileStream(_locationsFilePath, FileMode.OpenOrCreate, FileAccess.Read);
+    AddressLocations = JsonSerializer.Deserialize<ObservableCollection<AddressLocationEntityViewModel>>(fs);
+  }
+  // private async Task FillAddressLocations()
+  // {
+  //     using (FileStream fs = new FileStream(_locationsFilePath, FileMode.OpenOrCreate, FileAccess.Read))
+  //     {
+  //         //AppData.Locations = await JsonSerializer.DeserializeAsync<ListLocations>(fs); //Это не работало
+  //         AddressLocations = await JsonSerializer.DeserializeAsync<ObservableCollection<AddressLocationEntityViewModel>>(fs);
+  //
+  //         //MessageBox.Show(string.Format("Count: {0}", AppData.Locations.Locations.Count)); //Тут поле пустое == null
+  //         //ListLocations? loc = await JsonSerializer.DeserializeAsync<ListLocations>(fs);
+  //         //Console.WriteLine(loc.Locations.Count.ToString());
+  //     }
+  // }
+
+  private async Task UploadAddressLocations()
+  {
+    if (!Directory.Exists(_directoryDataResourcePath))
+    {
+      Directory.CreateDirectory(_directoryDataResourcePath);
+    }
+
+    JsonSerializerOptions options = new JsonSerializerOptions
+    {
+      Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+      WriteIndented = true
+    };
+
+    using (FileStream fs = new FileStream(_locationsFilePath, FileMode.OpenOrCreate))
+    {
+      if (AddressLocations != null)
+        await JsonSerializer.SerializeAsync<ObservableCollection<AddressLocationEntityViewModel>>(fs, AddressLocations,
+          options);
+    }
+  }
 }
--- src/Core/ClientExplorer.Shared.ViewModels/Entities/AddressLocationEntityViewModel.cs
+++ src/Core/ClientExplorer.Shared.ViewModels/Entities/AddressLocationEntityViewModel.cs
@@ -4,9 +4,9 @@ public sealed class AddressLocationEntityViewModel
 {
     #region Public Properties
 
-    public string? CityName { get; set; }
-    public string? StreetName { get; set; }
-    public string? HouseNumber { get; set; }
+    public string CityName { get; set; } = string.Empty;
+    public string StreetName { get; set; } = string.Empty;
+    public string HouseNumber { get; set; } = string.Empty;
 
     #endregion
 }
