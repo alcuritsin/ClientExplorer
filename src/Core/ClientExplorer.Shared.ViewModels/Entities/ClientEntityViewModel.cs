e2f00915646245cf4b71ccb70acdca45228187f7
Kuricyn Aleksej <al.curitsin@gmail.com>
2023-03-02 08:55:15 +0500

    Add Logo Client in ClientEntityViewModel Теперь если в папке клиента лежит файл .logo.png он используется в качестве логотипа. Если в папке такой файл отсутствует вместо логотипа встаёт "заглушка".

--- src/Core/ClientExplorer.Shared.ViewModels/Entities/ClientEntityViewModel.cs
+++ src/Core/ClientExplorer.Shared.ViewModels/Entities/ClientEntityViewModel.cs
@@ -9,13 +9,23 @@ namespace ClientExplorer.Shared.ViewModels;
 public class ClientEntityViewModel : BaseViewModel
 {
   public string? Name { get; set; }
-  public Bitmap IconPath { get; private set; }
+  public Bitmap Icon { get; private set; }
   public DirectoryInfo? ClientPath { get; set; }
+
   public ClientEntityViewModel(string clientName, DirectoryInfo clientDirectory)
   {
     Name = clientName;
     ClientPath = clientDirectory;
-    IconPath = new Bitmap(AvaloniaLocator.Current.GetService<IAssetLoader>()
-      .Open(new Uri($"avares://ClientExplorer/Assets/not-available.png")));
+    
+    var iconPath = clientDirectory.FullName + Path.DirectorySeparatorChar + ".logo.png";
+    if (File.Exists(iconPath))
+    {
+      Icon = new Bitmap(iconPath);
+    }
+    else
+    {
+      Icon = new Bitmap(AvaloniaLocator.Current.GetService<IAssetLoader>()
+        .Open(new Uri($"avares://ClientExplorer/Assets/not-available.png")));
+    }
   }
 }
