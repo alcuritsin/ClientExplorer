fc6adfb7fb640bb77e701f0ae2476200200c1f6a
Kuricyn Aleksej <al.curitsin@gmail.com>
2023-03-06 16:35:01 +0500

    In FolderLocationEntityViewModel.cs add new Property DirectoryEntity FolderDirectory. В FolderLocationEntityViewModel добавлено новое свойство DirectoryEntity

--- src/Core/ClientExplorer.Shared.ViewModels/Entities/FolderLocationEntityViewModel.cs
+++ src/Core/ClientExplorer.Shared.ViewModels/Entities/FolderLocationEntityViewModel.cs
@@ -1,3 +1,5 @@
+using ClientExplorer.Application;
+
 namespace ClientExplorer.Shared.ViewModels;
 
 public class FolderLocationEntityViewModel : BaseViewModel
@@ -5,9 +7,11 @@ public class FolderLocationEntityViewModel : BaseViewModel
   public string FolderName { get; set; } = string.Empty;
   public bool IsCheck { get; set; } = false;
   public bool IsEnable { get; set; } = true;
+  public DirectoryEntity FolderDirectory { get; set; }
 
-  public FolderLocationEntityViewModel(string folderName)
+  public FolderLocationEntityViewModel(string folderName, DirectoryEntity folderDirectory)
   {
     FolderName = folderName;
+    FolderDirectory = folderDirectory;
   }
 }
