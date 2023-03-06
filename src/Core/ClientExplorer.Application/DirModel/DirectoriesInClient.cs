27d922683abff9a44c20f14ec870b63cb0fd28f5
Kuricyn Aleksej <al.curitsin@gmail.com>
2023-03-06 16:30:05 +0500

    The constant is used as the name of the folder with client objects В качестве имени папки с объектами клиента использована константа

--- src/Core/ClientExplorer.Application/DirModel/DirectoriesInClient.cs
+++ src/Core/ClientExplorer.Application/DirModel/DirectoriesInClient.cs
@@ -18,7 +18,9 @@ public class DirectoriesInClient
     //TODO Вынести список папок в какой-то файл настроек. Для возможности редактирования. Пока список папок хардкодим. 
     Folders.Add(new DirectoryEntity("Документы общие"));
     Folders.Add(new DirectoryEntity("Каталог стандартных решений"));
-    Folders.Add(new DirectoryEntity("Объекты"));
+    Folders.Add(new DirectoryEntity(ClientEr.FolderObjectsName));
+
+
   }
   
 }
